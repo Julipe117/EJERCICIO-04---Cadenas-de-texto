364b97f9b5b5442d829de6137fab50456d326dd8
Julipe117 <110401271+Julipe117@users.noreply.github.com>
2022-09-02 11:57:45 +0200

    Add files via upload

---
+++ EJERCICIO 04.js
@@ -0,0 +1,24 @@
+let nombre = "Sergio";
+console.log(nombre);
+
+let apellido = "de Luque";
+console.log(apellido);
+
+let estudiante = `${nombre} ${apellido}`;
+console.log(estudiante);
+
+let estudianteMayus = estudiante.toUpperCase();
+console.log(estudianteMayus);
+
+let estudianteMinus = estudiante.toLowerCase();
+console.log(estudianteMinus);
+
+console.log(estudiante.length);
+console.log(nombre[0]);
+console.log(apellido[7]);
+
+console.log(estudiante.trim());
+
+console.log(estudiante.includes("Sergio"));
+
+
